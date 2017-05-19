207f8a13be4179a03e2e7ab3c2d2f37e08da3738
jmatarrita <jmatarrita@symbiotic.net>
2017-05-19 09:46:33 -0600

    first commit

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+//connect to server
+$connection = new MongoCLient("mongodb://admin:admin@10.1.4.133"); 
+
+//connect to a database 
+	$db = $connection -> Ciudades
+	selectDB("Monitoreo")
+ 
+
+?>
---
+++ sortingThrough.php
@@ -0,0 +1,5 @@
+<?php
+$cursor->sort(array('fecha'=>"10/12/2015 00:00"));
+ 
+
+?>
