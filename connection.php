c049d5f949bf61ab4e5d9cc1d03568f0d962be82
Daniel <daa.gonzalez@gmail.com>
2017-05-25 17:03:52 -0600

    Consultas parametrizadas

--- connection.php
+++ connection.php
@@ -1,16 +1,29 @@
 <?php
-require 'vendor/autoload.php';
-//connect to server
-$connection = new MongoDB\Client("mongodb://admin:admin2016@10.1.4.133:27017"); 
+	require 'vendor/autoload.php';
+	//connect to server
+	$connection = new MongoDB\Client("mongodb://admin:admin2016@10.1.4.133:27017");
 
-//connect to a database 
+	//connect to a database
 	$db = $connection->jm;
-	$collection = $db->Marguito;
-    
-        // var_dump($doc);
-    $dateQuery = array('fecha' => "1/01/2015 00:0", 'fecha'=>"10/12/2015 00:00", 'nombre' => "Cascada Azul");
+	$collection = $db->sitiosMuestreo;
 
-    $cursor = $collection->find($dateQuery);
-    echo json_encode(iterator_to_array($cursor));
+	if (isset($_GET['campo'])) {
+        $campo = $_GET['campo'];
+    }else{
+        // Fallback behaviour goes here
+    }
+
+		if ($campo == "nombre") {
+			$nombre = "Cascada Azul";
+			$mongoQuery = array('nombre'=> $nombre);
+		}
+		else if ($campo == "fechas") {
+			$fechaIni = "1/01/2015 00:0";
+			$fechaFin = "10/12/2015 00:00";
+			$mongoQuery = array('fecha' => array('$gt' => $fechaIni, '$lte' => $fechaFin));
+		}
+
+	$cursor = $collection->find($mongoQuery);
+	echo json_encode(iterator_to_array($cursor));
 
 ?>
